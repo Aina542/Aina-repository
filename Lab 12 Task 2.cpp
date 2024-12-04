cf8b3763d1b9497387befbae87d58328743a5272
Ainaktk <mahadktk405@gmail.com>
2024-12-03 21:17:33 -0800

    Add files via upload

---
+++ Lab 12 Task 2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string str = "Hello, World!";
+	int length = str.length();
+	cout<< "The length of the string is:" <<length <<endl;
+	return 0;
+}
